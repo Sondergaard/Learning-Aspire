b70b9ee6422337f9ab4430bb976296f47b0f1460
sondergaard <s.sondergaard@icloud.com>
2026-01-19 14:08:22 +0100

    Increase default timeout for integration tests to 5 minutes

--- TodoFuncEndpointTests/IntegrationTest1.cs
+++ TodoFuncEndpointTests/IntegrationTest1.cs
@@ -4,7 +4,7 @@ namespace TodoFuncEndpointTests;
 
 public class IntegrationTest1
 {
-    private static readonly TimeSpan DefaultTimeout = TimeSpan.FromSeconds(30);
+    private static readonly TimeSpan DefaultTimeout = TimeSpan.FromMinutes(5);
 
     [Fact]
     public async Task GetWebResourceRootReturnsOkStatusCode()
